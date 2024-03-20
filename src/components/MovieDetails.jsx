715fc75836bc2ed96abf3a92b7cdba1d6f86963d
Ilaria Rosati <156794733+Estethell@users.noreply.github.com>
2024-03-20 17:05:21 +0100

    commit

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -9,9 +9,8 @@ const MovieDectails = () => {
   const params = useParams();
 
   const fetchMovies = () => {
-    debugger;
-    const url = "http://www.omdbapi.com/?i=tt3896198&apikey=e8901073&s=";
-    fetch(url + params.movieId)
+    const url = "http://www.omdbapi.com/?apikey=e8901073&i=";
+    fetch(url + params.imdbID)
       .then((response) => {
         if (response.ok) {
           return response.json();
@@ -22,7 +21,7 @@ const MovieDectails = () => {
       .then((apiItems) => {
         // this.setState({ arrayOfMovies: ApiItems.Search.slice(0, 6), loader: false });
         const rightMovieToShow = apiItems.find((b) => b.imdbID === parseInt(params.movieId));
-        debugger;
+
         if (rightMovieToShow) {
           setMovieObj(rightMovieToShow);
         }
@@ -34,7 +33,6 @@ const MovieDectails = () => {
 
   useEffect(() => {
     fetchMovies();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [params.movieId]);
 
   return (
